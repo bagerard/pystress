0e1aac7925fc13918852ef51e1558d3abe27bd54
Bastien Gerard <bastien.gerard@cluepoints.com>
2020-12-10 18:36:25 +0100

    remove python 2.6 as completely outdated and added python 3.9 in travisq

--- setup.py
+++ setup.py
@@ -9,10 +9,8 @@ def get_version():
 
 
 def get_long_description():
-    desc = None
     with open('README.rst') as f:
-        desc = f.read()
-    return desc
+        return f.read()
 
 
 setup(
